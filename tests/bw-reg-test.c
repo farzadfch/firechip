a593a73136713f5af6ca8155a4fb63f44b333429
Farzad Farshchi <farzadfr@gmail.com>
2019-05-16 14:56:08 -0500

    bw-reg-test supports coalescing

--- tests/bw-reg-test.c
+++ tests/bw-reg-test.c
@@ -1,18 +1,25 @@
 #include <stdint.h>
 #include "mmio.h"
 
+#define N 4
 #define BW_REG_BASE 0x20000000L
-#define WINDOW_SIZE (BW_REG_BASE)
-#define MAX_XACTION(i) (BW_REG_BASE + (i+1)*0x4)
+#define ENABLE_BW (BW_REG_BASE)
+#define WINDOW_SIZE (BW_REG_BASE + 0x4)
+#define MAX_XACTION(i) (BW_REG_BASE + (2+i)*0x4)
+#define ENABLE_MASTERS (BW_REG_BASE + (2+N)*0x4)
+#define DOMAIN_ID(i) (BW_REG_BASE + (3+N+i)*0x4)
 
 
-#define N (16 * 1024 / 8 * 2)
-volatile uint64_t values[N];
+#define WSS_MAX (16 * 1024 / 8 * 2)
+volatile uint64_t values[WSS_MAX];
 
 int main(void)
 {
   reg_write32(WINDOW_SIZE, 500-1);
   reg_write32(MAX_XACTION(0), 1);
+  reg_write32(DOMAIN_ID(0), 0);
+  reg_write32(ENABLE_MASTERS, 1 << 0);
+  reg_write32(ENABLE_BW, 1);
   asm volatile ("fence");
 
   for (int j = 0; j < 1; j++)
