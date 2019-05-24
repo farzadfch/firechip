e206b2787db9c2596d0c7d9a8c7f6be127ca9040
Farzad Farshchi <farzadfr@gmail.com>
2019-05-24 18:06:29 -0500

    wss and iterations can be changed more easily in bandwidth.c

--- tests/bandwidth.c
+++ tests/bandwidth.c
@@ -9,8 +9,10 @@
 #define MAX_XACTION(i) (BW_REG_BASE + (i+1) * 0x4)
 
 #define NC_MAX 4
-// L1 cache size * 2
-#define WSS_MAX (16 * 1024 / 8 * 2)
+// 64 byte block
+#define BLK_SZ 8
+// L1 cache size * 2, L1 = 16k
+#define WSS_MAX (16 * 1024 / BLK_SZ * 2)
 
 volatile uint64_t values0[WSS_MAX];
 volatile uint64_t values1[WSS_MAX];
@@ -18,13 +20,15 @@ volatile uint64_t values2[WSS_MAX];
 volatile uint64_t values3[WSS_MAX];
 uint64_t cycle1[NC_MAX];
 uint64_t cycle2[NC_MAX];
-int run_core_mask[NC_MAX] = {1, 1, 1, 1};
-#define NC 4
+int run_core_mask[NC_MAX] = {0, 0, 0, 1};
+#define NC 1
 #define PRINT_CORE 3
 
 void thread_entry(int cid, int nc)
 {
   volatile register uint64_t *values;
+  register int itr;
+  register int wss;
 
   while (run_core_mask[cid] == 0);
   if (cid == PRINT_CORE)
@@ -32,13 +36,25 @@ void thread_entry(int cid, int nc)
 
   switch (cid)
   {
-    case 0: values = values0;
+    case 0:
+      values = values0;
+      itr = 1;
+      wss = 64 * BLK_SZ;
       break;
-    case 1: values = values1;
+    case 1:
+      values = values1;
+      itr = 1;
+      wss = 64 * BLK_SZ;
       break;
-    case 2: values = values2;
+    case 2:
+      values = values2;
+      itr = 1;
+      wss = 64 * BLK_SZ;
       break;
-    case 3: values = values3;
+    case 3:
+      values = values3;
+      itr = 1;
+      wss = 64 * BLK_SZ;
       break;
   }
 
@@ -48,8 +64,10 @@ void thread_entry(int cid, int nc)
   register uint64_t cycle1_reg = rdcycle();
 
   // This is the core of the benchmark
-  for (int i = 0; i < 64 * 8; i = i + 8) {
-    values[i];
+  for (int j = 0; j < itr; j++) {
+    for (int i = 0; i < wss; i = i + BLK_SZ) {
+      values[i];
+    }
   }
 
   cycle2[cid] = rdcycle();
