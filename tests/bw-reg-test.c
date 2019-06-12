7c775375f3013454bc2a17b543e429781db9586e
Farzad Farshchi <farzadfr@gmail.com>
2019-06-12 17:52:29 -0500

    Modifies bw-reg-tests for the new bw-reg. Bumps for rocket chip.

--- tests/bw-reg-test.c
+++ tests/bw-reg-test.c
@@ -3,11 +3,12 @@
 
 #define N 4
 #define BW_REG_BASE 0x20000000L
-#define ENABLE_BW_COST (BW_REG_BASE)
+#define ENABLE_BW (BW_REG_BASE)
 #define WINDOW_SIZE (BW_REG_BASE + 0x4)
-#define MAX_XACTION(i) (BW_REG_BASE + (2+i)*0x4)
-#define ENABLE_MASTERS (BW_REG_BASE + (2+N)*0x4)
-#define DOMAIN_ID(i) (BW_REG_BASE + (3+N+i)*0x4)
+#define MAX_READ(i) (BW_REG_BASE + (2+i)*0x4)
+#define MAX_WRITE(i) (BW_REG_BASE + (2+N+i)*0x4)
+#define ENABLE_MASTERS (BW_REG_BASE + (2+2*N)*0x4)
+#define DOMAIN_ID(i) (BW_REG_BASE + (3+2*N+i)*0x4)
 
 
 #define WSS_MAX (16 * 1024 / 8 * 2)
@@ -15,11 +16,12 @@ volatile uint64_t values[WSS_MAX];
 
 int main(void)
 {
-  reg_write32(WINDOW_SIZE, 100-1);
-  reg_write32(MAX_XACTION(0), 3);
+  reg_write32(WINDOW_SIZE, 200-1);
+  reg_write32(MAX_READ(0), 2);
+  reg_write32(MAX_WRITE(0), 1);
   reg_write32(DOMAIN_ID(0), 0);
   reg_write32(ENABLE_MASTERS, 1 << 0);
-  reg_write32(ENABLE_BW_COST, 1 | 1 << 1 | 1 << 3);
+  reg_write32(ENABLE_BW, 1);
   asm volatile ("fence");
 
   for (int i = 0; i < WSS_MAX / 8; i = i + 8)
