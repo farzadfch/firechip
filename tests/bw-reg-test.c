2fb1c615e542f506f35d2edf5695c3002757bc96
Farzad Farshchi <farzadfr@gmail.com>
2019-07-12 23:33:37 +0000

    Adapt bw-reg-test for new design. Bump for rocket chip.

--- tests/bw-reg-test.c
+++ tests/bw-reg-test.c
@@ -5,10 +5,9 @@
 #define BW_REG_BASE 0x20000000L
 #define ENABLE_BW (BW_REG_BASE)
 #define WINDOW_SIZE (BW_REG_BASE + 0x4)
-#define MAX_READ(i) (BW_REG_BASE + (2+i)*0x4)
-#define MAX_WRITE(i) (BW_REG_BASE + (2+N+i)*0x4)
-#define ENABLE_MASTERS (BW_REG_BASE + (2+2*N)*0x4)
-#define DOMAIN_ID(i) (BW_REG_BASE + (3+2*N+i)*0x4)
+#define MAX(i) (BW_REG_BASE + (2+i)*0x4)
+#define ENABLE_MASTERS (BW_REG_BASE + (2+N)*0x4)
+#define DOMAIN_ID(i) (BW_REG_BASE + (3+N+i)*0x4)
 
 
 #define WSS_MAX (16 * 1024 / 8 * 2)
@@ -17,11 +16,10 @@ volatile uint64_t values[WSS_MAX];
 int main(void)
 {
   reg_write32(WINDOW_SIZE, 200-1);
-  reg_write32(MAX_READ(0), 2);
-  reg_write32(MAX_WRITE(0), 1);
+  reg_write32(MAX(0), 2);
   reg_write32(DOMAIN_ID(0), 0);
   reg_write32(ENABLE_MASTERS, 1 << 0);
-  reg_write32(ENABLE_BW, 1);
+  reg_write32(ENABLE_BW, 1 | 1 << 1);
   asm volatile ("fence");
 
   for (int i = 0; i < WSS_MAX / 8; i = i + 8)
