7bdce2abf3eae11c34d156d791f57014ae8c2392
Farzad Farshchi <farzadfr@gmail.com>
2019-08-05 19:01:18 -0500

    bump for rocket chip and adapt bw-reg-test

--- tests/bw-reg-test.c
+++ tests/bw-reg-test.c
@@ -7,8 +7,9 @@
 #define BW_SETTINGS (BW_REG_BASE + 1*0x4)
 #define WINDOW_SIZE (BW_REG_BASE + 2*0x4)
 #define MAX(i) (BW_REG_BASE + (3+i)*0x4)
-#define ENABLE_MASTERS (BW_REG_BASE + (3+N)*0x4)
-#define DOMAIN_ID(i) (BW_REG_BASE + (4+N+i)*0x4)
+#define MAX_WR(i) (BW_REG_BASE + (3+N+i)*0x4)
+#define ENABLE_MASTERS (BW_REG_BASE + (3+2*N)*0x4)
+#define DOMAIN_ID(i) (BW_REG_BASE + (4+2*N+i)*0x4)
 
 
 #define WSS_MAX (16 * 1024 / 8 * 4)
@@ -18,6 +19,7 @@ int main(void)
 {
   reg_write32(WINDOW_SIZE, 400-1);
   reg_write32(MAX(0), 1);
+  reg_write32(MAX_WR(0), 1);
   reg_write32(DOMAIN_ID(0), 0);
   reg_write32(ENABLE_MASTERS, 1 << 0);
   reg_write32(BW_SETTINGS, 3);
